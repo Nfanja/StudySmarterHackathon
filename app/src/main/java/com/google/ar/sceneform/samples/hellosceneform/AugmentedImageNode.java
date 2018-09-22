12bdecf68c800402d480871143d92e7571d1ac32
Fedor Fedoseev <fedor.fedoseev@tum.de>
2018-09-22 23:49:28 +0200

    augmented images are detected

--- app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/AugmentedImageNode.java
+++ app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/AugmentedImageNode.java
@@ -21,12 +21,10 @@ public class AugmentedImageNode extends AnchorNode {
     private AugmentedImage image;
     private static CompletableFuture<ModelRenderable> modelFuture;
 
-    public AugmentedImageNode(Context context, String filename) {
+    public AugmentedImageNode(Context context, int resource) {
         // Upon construction, start loading the modelFuture
         if (modelFuture == null) {
-            modelFuture = ModelRenderable.builder().setRegistryId("modelFuture")
-                    .setSource(context, Uri.parse("models/" + filename))
-                    .build();
+            modelFuture = ModelRenderable.builder().setSource(context, resource).build();
         }
     }
 
--- app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/MyArFragment.java
+++ app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/MyArFragment.java
@@ -1,5 +1,7 @@
 package com.google.ar.sceneform.samples.hellosceneform;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -23,22 +25,43 @@ public class MyArFragment extends ArFragment {
     public static String TAG = MyArFragment.class.getSimpleName();
     public boolean hosting = false;
 
+    private Bitmap loadImage(String name) {
+        Bitmap bitmap = null;
+        try (InputStream inputStream = this.getContext().getAssets().open("augmented-images-earth.jpg")) {
+            bitmap = BitmapFactory.decodeStream(inputStream);
+        } catch (IOException e) {
+            Log.e(TAG, "I/O exception loading augmented image bitmap.", e);
+        }
+
+        return bitmap;
+    }
+
     @Override
     public void onResume() {
         super.onResume();
-        InputStream inputStream = null;
+        AugmentedImageDatabase imageDatabase;
 
         Session session = getArSceneView().getSession();
 
-        try {
-            inputStream = this.getContext().getAssets().open("example.imgdb");
-            AugmentedImageDatabase imageDatabase = AugmentedImageDatabase.deserialize(session, inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         Config config = new Config(session);
+        config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
         config.setCloudAnchorMode(Config.CloudAnchorMode.ENABLED);
+
+        imageDatabase = new AugmentedImageDatabase(session);
+        Bitmap earth = loadImage("augmented-images-earth.jpg");
+        Bitmap hydrogen = loadImage("H.jpg");
+        Bitmap oxygen = loadImage("O.jpg");
+        Bitmap cl = loadImage("Cl.jpg");
+        Bitmap na = loadImage("na.jpg");
+
+        imageDatabase.addImage("earth", earth);
+        imageDatabase.addImage("oxygen", oxygen);
+        imageDatabase.addImage("hydrogen", hydrogen);
+        imageDatabase.addImage("Cl", cl);
+        imageDatabase.addImage("Na", na);
+
+        config.setAugmentedImageDatabase(imageDatabase);
+
         session.configure(config);
         try {
             session.resume();
@@ -51,46 +74,51 @@ public class MyArFragment extends ArFragment {
     public void onUpdate(FrameTime frameTime) {
         super.onUpdate(frameTime);
         Session session = getArSceneView().getSession();
-        if (session != null && anchor != null && hosting) {
-            try {
-                Frame frame = session.update();
-                frame.getCamera().getTrackingState();
-                Anchor.CloudAnchorState state = anchor.getCloudAnchorState();
-                if(state.isError()){
-                    Log.e(TAG, "Error: " + state);
-                    Toast.makeText(this.getContext(), "Error", Toast.LENGTH_LONG).show();
-                    hosting = false;
+        if (session != null) {
+            // Augmented images
+//            try {
+                Frame frame = getArSceneView().getArFrame();
+                Collection<AugmentedImage> updatedAugmentedImages =
+                        frame.getUpdatedTrackables(AugmentedImage.class);
 
-                }else if(state == Anchor.CloudAnchorState.SUCCESS){
-                    Log.e(TAG, "ID: " + anchor.getCloudAnchorId());
-                    Toast.makeText(this.getContext(), "Host successfull", Toast.LENGTH_LONG).show();
-                    hosting = false;
-
-                    // Augmented images
-                    Collection<AugmentedImage> updatedAugmentedImages =
-                            frame.getUpdatedTrackables(AugmentedImage.class);
-
-                    for (AugmentedImage img : updatedAugmentedImages) {
-                        // Developers can:
-                        // 1. Check tracking state.
-                        // 2. Render something based on the pose, or attach an anchor.
-                        if (img.getTrackingState() == TrackingState.TRACKING) {
-                            // You can also check which image this is based on getName().
-//                            if (img.getIndex() == dogIndex) {
-//                                // TODO: Render a 3D version of a dog in front of img.getCenterPose().
-//                            } else if (img.getIndex() == catIndex) {
-//                                // TODO: Render a 3D version of a cat in front of img.getCenterPose().
-//                            }
-                            AugmentedImageNode node = new AugmentedImageNode(this.getContext(), "jetengine.sfa");
-                            node.setImage(img);
-                            getArSceneView().getScene().addChild(node);
-                        }
+                for (AugmentedImage img : updatedAugmentedImages) {
+                    // Developers can:
+                    // 1. Check tracking state.
+                    // 2. Render something based on the pose, or attach an anchor.
+                    if (img.getTrackingState() == TrackingState.TRACKING) {
+                        // You can also check which image this is based on getName().
+                        //                            if (img.getIndex() == dogIndex) {
+                        //                                // TODO: Render a 3D version of a dog in front of img.getCenterPose().
+                        //                            } else if (img.getIndex() == catIndex) {
+                        //                                // TODO: Render a 3D version of a cat in front of img.getCenterPose().
+                        //                            }
+                        AugmentedImageNode node = new AugmentedImageNode(this.getContext(), R.raw.hydrogen);
+                        node.setImage(img);
+                        getArSceneView().getScene().addChild(node);
                     }
                 }
-
-            } catch (CameraNotAvailableException e) {
-                e.printStackTrace();
-            }
+//            } catch (CameraNotAvailableException e) {
+//                e.printStackTrace();
+//            }
+//            try {
+//                Frame frame = session.update();
+//                frame.getCamera().getTrackingState();
+//                Anchor.CloudAnchorState state = anchor.getCloudAnchorState();
+//                if(state.isError()){
+//                    Log.e(TAG, "Error: " + state);
+//                    Toast.makeText(this.getContext(), "Error", Toast.LENGTH_LONG).show();
+//                    hosting = false;
+//
+//                }else if(state == Anchor.CloudAnchorState.SUCCESS){
+//                    Log.e(TAG, "ID: " + anchor.getCloudAnchorId());
+//                    Toast.makeText(this.getContext(), "Host successfull", Toast.LENGTH_LONG).show();
+//                    hosting = false;
+//
+//                }
+//
+//            } catch (CameraNotAvailableException e) {
+//                e.printStackTrace();
+//            }
         }
     }
 
